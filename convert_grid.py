cee7518b8bf19c36c17c4c9c56ece450f95acb40
Tom Langford <citemysource@hotmail.com>
2018-02-26 22:16:25 +0000

    Moved saving the csv to a sudoku_utilities.

--- convert_grid.py
+++ convert_grid.py
@@ -15,9 +15,8 @@ ToDo:
 """
 
 import argparse
-import csv
 from os.path import basename, join, splitext
-from sudoku_utils import print_sudoku
+from sudoku_utils import print_sudoku, save_sudoku
 
 
 def convert_sudoku_to_csv(file_name, view=False):
@@ -34,7 +33,7 @@ def convert_sudoku_to_csv(file_name, view=False):
 
     """
     csv_name = splitext(basename(file_name))[0] + ".csv"
-    view_grid = []
+    sudoku_grid = []
 
     # Set input and output file paths
     input_path = join("./raw_sudoku", basename(file_name))
@@ -42,25 +41,21 @@ def convert_sudoku_to_csv(file_name, view=False):
 
     # Open input file and create output file
     with open(input_path, "r") as input_file:
-        with open(output_path, "w+", newline='') as output_file:
-            csv_writer = csv.writer(output_file, delimiter=",")
-            for i, line in enumerate(input_file):
-                # Drop lines begining with "-"
-                if line[0] == "-":
-                    continue
-                number_line = []
-                # Convert numerics to int or add 0 to represent blanks
-                for char in line:
-                    if char.isdigit():
-                        number_line.append(int(char))
-                    elif char == ".":
-                        number_line.append(0)
-                # Ensure that the line length = 9
-                assert len(number_line) == 9
-                view_grid = view_grid + number_line
-                csv_writer.writerow(number_line)
+        for i, line in enumerate(input_file):
+            # Drop lines begining with "-"
+            if line[0] == "-":
+                continue
+            # Convert numerics to int or add 0 to represent blanks
+            for char in line:
+                if char.isdigit():
+                    sudoku_grid.append(int(char))
+                elif char == ".":
+                    sudoku_grid.append(0)
+    # Ensure that grid contains 81 numbers
+    assert len(sudoku_grid) == 81, "Incorrect total of numbers in grid."
     if view:
-        print_sudoku(view_grid)
+        print_sudoku(sudoku_grid)
+    save_sudoku(sudoku_grid, output_path)
 
 
 if __name__ == "__main__":
--- sudoku_utils.py
+++ sudoku_utils.py
@@ -5,9 +5,15 @@ Contains functions shared across the Genetic Algorithm Sudoku project.
 Example:
     from sudoku_utils import print_sudoku
 """
+import csv
 
 
 def print_row(row):
+    """Print each char in a list.
+
+    args:
+        row (list) list representing a row of a Sudoku grid
+    """
     for c in row:
         print(c, end=" ", flush=True)
     print()
@@ -27,8 +33,20 @@ def print_sudoku(sudoku_gene):
             print_row(row)
             row_count += 1
             if (row_count) % 3 == 0 and row_count < 9:
-                hash_line = ["-" for i in range(11)]
-                print_row(hash_line)
+                dash_line = ["-" for i in range(11)]
+                print_row(dash_line)
             row = []
         elif (i + 1) % 3 == 0:
             row.append("|")
+
+
+def save_sudoku(sudoku_gene, output_path):
+    """Save Sudoku as a csv."""
+    with open(output_path, "w+", newline='') as output_file:
+        csv_writer = csv.writer(output_file, delimiter=",")
+        number_line = []
+        for i, number in enumerate(sudoku_gene):
+            number_line.append(number)
+            if (i + 1) % 9 == 0:
+                csv_writer.writerow(number_line)
+                number_line = []
