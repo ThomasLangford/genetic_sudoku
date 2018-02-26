aab02e9fe76dde2aed455f052431b292f1cde055
Tom Langford <citemysource@hotmail.com>
2018-02-26 18:07:42 +0000

    Fixed EOL warning in solve_sudoku.py

--- solve_sudoku.py
+++ solve_sudoku.py
@@ -1,9 +1,9 @@
 """Solve a given Sudoku problem using Genetic Algorithms.
 
 This program uses a Genetic Algorithm to solve a specified Sudoku problem
-contained within a csv file. The found solution or best solution if the stopping
-criteria has been reached will be displayed on screen along with the fitness
-score. It will then be saved as a csv file in the solved_sudoku folder.
+contained within a csv file. The found solution or best solution if the
+stopping criteria has been reached will be displayed on screen along with the
+fitness score. It will then be saved as a csv file in the solved_sudoku folder.
 
 Flags:
     -i --input Name of the grid file in csv_sudoku to solve.
